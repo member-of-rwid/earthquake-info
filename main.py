a65ef1feee85e8b5ba34835dfab09a64fc4b61bc
fatihlhz <alfatih.hergun@gmail.com>
2022-04-08 14:31:06 +0700

    Updated

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ def extract_data():
     update = dict()
     update["date"] = "7 April 2022"
     update["time"] = "16:52:58"
+    update["magnitude"] = 5.1
     update["depth"] = 118
     update["location"] = {"LU": 6.01, "BT": 125.79}
     update["source"] = "244 km BaratLaut MELONGUANE-SULUT"
     return update
 
 
-def visualize_data():
+def visualize_data(result):
     print("Latest Earthquake of Indonesia")
     print(f"Date : {result['date']}")
     print(f"Time : {result['time']}")
+    print(f"Magnitude : {result['magnitude']}")
     print(f"Depth : {result['depth']}")
-    print(f"Location : {result['location']}")
+    print(f"Location : LU = {result['location']['LU']}, BT = {result['location']['BT']}")
     print(f"Source : {result['source']}")
 
 
 if __name__ == '__main__':
     print("Main Apps")
-    result = extract_data()
-    visualize_data(result)
+    final_result = extract_data()
+    visualize_data(final_result)
